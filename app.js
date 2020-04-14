05cab43058873c3c7c3b791c6cc09ee59d58eeaa
pjmantoss <joelptoss@gmail.com>
2020-04-14 18:48:50 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
+    //card options
+
+    const cardArray = [
+        {
+            name: 'cheeseburger',
+            img: './images/cheeseburger.png'
+        },
+        {
+            name: 'fries',
+            img: './images/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: './images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: './images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: './images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: './images/pizza.png'
+        }
+    ];
+
+    const grid = document.querySelector('.grid');
+
+    // game board
 })
