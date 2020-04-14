e4093a1198a1037cac9ca7201ba00bace2880ed3
pjmantoss <joelptoss@gmail.com>
2020-04-14 19:15:45 +0100

    update app.js

--- app.js
+++ app.js
@@ -54,16 +54,27 @@ document.addEventListener("DOMContentLoaded", () => {
     ]
 
     const grid = document.querySelector('.grid');
+    let cardChosen = [];
+    let cardChosenId = [];
 
     // game board
      function createBoard(){
-        for (let i = o; i < cardArray.length; i++){
+        for (let i = 0; i < cardArray.length; i++){
             let card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
             card.setAttribute('data-1', i);
-            //card.addEventListener('click', flipcard);
+            card.addEventListener('click', flipCard);
             grid.appendChild(card);
         }
+    }
+
+    //check for matches
+
+    //Flip card
+    function flipCard(){
+        let cardId = this.getAttribute('data-id');
+        cardChosen.push(cardArray[cardId].name);
+        cardChosenId.push(cardId);
     }
 
     createBoard();
